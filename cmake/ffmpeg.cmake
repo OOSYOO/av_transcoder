9bac5cc3040cce57f3bbea11438545e8a4d1f583
OOSYOO <381940186@qq.com>
2021-04-09 09:23:49 +0800

    build success on centos

--- cmake/ffmpeg.cmake
+++ cmake/ffmpeg.cmake
@@ -1,6 +1,12 @@
+
+if(UNIX)
+MESSAGE(STATUS “This is Linux.”)
+include_directories(${CMAKE_SOURCE_DIR}/3rdparty/ffmpeg/include)
+link_directories(${CMAKE_SOURCE_DIR}/3rdparty/ffmpeg/libcentos)
+elseif(APPLE)
+MESSAGE(STATUS “This is APPLE.”)
 include_directories(/usr/local/include)
 link_directories(/usr/local/lib)
-# include_directories(${CMAKE_SOURCE_DIR}/3rdparty/ffmpeg/include)
-# link_directories(${CMAKE_SOURCE_DIR}/3rdparty/ffmpeg/lib)
+endif()
 
-set(FFMPEG_LIBRARIES avformat avfilter avcodec avutil)
+set(FFMPEG_LIBRARIES avformat avfilter avcodec avutil swresample swscale z bz2)
